de877fc1b69f873c5c41d63572ce36150a168640
hongsa <hsasung@gmail.com>
2016-11-29 18:44:18 +0900

    add changelog

--- src/app/common/filters/subscribe.filter.js
+++ src/app/common/filters/subscribe.filter.js
@@ -1,8 +1,8 @@
 (function () {
   'use strict';
   var subscribeNames = {
-    'newUser': '신규 구독',
-    'continueUser': '누적 구독',
+    'newUser': '신규 구독 (환불 제외)',
+    'continueUser': '누적 구독 (구독 해지 제외)',
     'stopUser':'구독 해지',
     'refundUser' : '환불',
     'totalUser' : '전체 구독'
